bf0a05f446758081ea2e5e22a5dcde8c917ed89a
muhammadjafar <ibook2025com@gmail.com>
2023-09-14 16:30:08 +0300

    init

---
+++ src/main/java/mit/ibook/btech/mit/MitApplication.java
@@ -0,0 +1,13 @@
+package mit.ibook.btech.mit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mit/ibook/btech/mit/MitApplicationTests.java
@@ -0,0 +1,13 @@
+package mit.ibook.btech.mit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
